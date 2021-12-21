69002b7da30de0930c28fb17daebf4d5f265b686
lishuai-1 <lishuai-1@thunisoft.com>
2021-12-21 09:26:41 +0800

    task init

---
+++ Tasks/text_classification/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python  
+#-*- coding:utf-8 _*-  
+""" 
+@author: scuislishuai 
+@license: Apache Licence 
+@file: __init__.py.py 
+@time: 2021/12/12
+@contact: scuislishuai@gmail.com
+@site:  
+@software: PyCharm 

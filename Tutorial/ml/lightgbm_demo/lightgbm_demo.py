72f12ff20f2e5e26b2fd916a5a234172539c3010
lishuai-1 <lishuai-1@thunisoft.com>
2022-02-21 17:54:52 +0800

    新增xgboost&lightgbm的demo

---
+++ Tutorial/ml/lightgbm_demo/lightgbm_demo.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python  
+# -*- coding:utf-8 -*-  
+""" 
+@author: scuisqianfeng 
+@license: Apache Licence 
+@file: lightgbm_demo.py 
+@time: 2022/02/21
+@contact: scuislishuai@gmail.com
+@site:  
+@software: PyCharm 
+"""
